537c2f18596a7928dc890c70e19898010d56f3da
orgil <c0a23006cb@edu.teu.ac.jp>
2024-05-07 15:21:40 +0900

    Renshuu4

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -156,9 +156,12 @@ def main():
         if bomb is not None:
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                fonto = pg.font.Font(None,80)
+                txt = fonto.render("Game Over", True,(255,0,0))
+                screen.blit(txt, [WIDTH/2-150, HEIGHT/2])
                 bird.change_img(8, screen)
                 pg.display.update()
-                time.sleep(1)
+                time.sleep(5)
                 return
         if not (beam is None or bomb is None):
             if beam.rct.colliderect(bomb.rct):  # ビームと爆弾が衝突したら
